34896e91a25eddf678cd9520542a0ad08e769fa1
Santiago Carrasco <jsanticc@hotmail.com>
2021-05-20 14:43:12 -0700

    Retrieves the last tweet from Elon Musk

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,13 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+export const config = {
+  twitter: {
+    BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN,
+    URL_TWEETS_ELON: process.env.TWITTER_URL_TWEETS_ELON,
+  },
+  telegram: {
+    BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  },
+};
--- src/ts/env.d.ts
+++ src/ts/env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     TELEGRAM_BOT_TOKEN: string;
+    TWITTER_BEARER_TOKEN: string;
+    TWITTER_URL_TWEETS_ELON: string;
   }
 }
